95d98472ef8e7691eea0f806b024b3903335bf07
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-07-20 20:53:06 -0400

    fixed mobile menu

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $("#toggle").on("click", function() {
-        ($(".item").toggleClass("active"));
-    });
-});
+let toggle = document.getElementById("toggle");
+let menu = document.getElementsByTagName("a");
+
+toggle.onclick = function() {
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i].style.display == 'block') {
+            menu[i].style.display = 'none';
+        } else {
+            menu[i].style.display = 'block';
+        }
+    }
+};
