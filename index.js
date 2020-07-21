aab5d3e1788940021ef316b1d3f967d67caf2e68
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-07-20 21:26:46 -0400

    rearranged components in mobile

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ toggle.onclick = function() {
     for (let i = 0; i < menu.length; i++) {
         if (menu[i].style.display == 'block') {
             menu[i].style.display = 'none';
+
         } else {
             menu[i].style.display = 'block';
         }
