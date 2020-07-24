bd08ac2a3326cc629e2102e357fd3bba1fb1bd19
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-07-24 02:35:19 -0400

    changed home page to include more info and a header

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 let toggle = document.getElementById("toggle");
 let menu = document.getElementsByTagName("a");
 
+var flkty = new Flickity( '.carousel', {
+    // options
+    wrapAround: true, 
+    freeScroll: true,
+    pageDots: false,
+    arrowShape: { 
+        x0: 35,
+        x1: 20, y1: 25,
+        x2: 70, y2: 50,
+        x3: 70
+    },
+});
+
 toggle.onclick = function() {
     for (let i = 0; i < menu.length; i++) {
         if (menu[i].style.display == 'block') {
