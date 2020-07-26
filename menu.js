06e5e05a4100299355fa355ec3086e9110611f35
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-07-26 18:47:52 -0400

    set up recipes page

---
+++ menu.js
@@ -0,0 +1,13 @@
+let toggle = document.getElementById("toggle");
+let menu = document.getElementsByTagName("a");
+
+toggle.onclick = function() {
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i].style.display == 'block') {
+            menu[i].style.display = 'none';
+
+        } else {
+            menu[i].style.display = 'block';
+        }
+    }
+};
