8f6533b692c3c8972c5fc992c3ae73b26dd53d60
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-08-03 12:19:20 -0400

    fixed menu issue with home page on mobile

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 let menu = document.getElementsByTagName("a");
+let toggle = document.getElementById("toggle");
+
+toggle.onclick = function() {
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i].style.display == 'block') {
+            menu[i].style.display = 'none';
+
+        } else {
+            menu[i].style.display = 'block';
+        }
+    }
+};
 
 var flkty = new Flickity( '.carousel', {
     // options
